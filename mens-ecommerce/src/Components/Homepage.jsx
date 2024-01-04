2969614be3612ce6c3fa786ad0512148bedc4af2
prateekrai1 <prateek@gmail.com>
2024-01-04 01:11:28 -0500

    sidebar

---
+++ mens-ecommerce/src/Components/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Navbar/Navbar'
+
+const Homepage = () => {
+  return (
+    <div className='home'>
+        <Navbar/>
+    </div>
+  )
+}
+
+export default Homepage

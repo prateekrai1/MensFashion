364df0a1ef5e58262821209b78f49bdeaeda2433
prateekrai1 <prateek@gmail.com>
2024-01-07 00:45:52 -0500

    carousel

--- mens-ecommerce/src/Components/Homepage.jsx
+++ mens-ecommerce/src/Components/Homepage.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Navbar from './Navbar/Navbar'
+import { BrowserRouter as Router } from 'react-router-dom'
+import CarouselMenu from './Carousel/CarouselMenu'
 
 const Homepage = () => {
   return (
     <div className='home'>
+      <Router>
         <Navbar/>
+        <CarouselMenu/>
+      </Router>
+      
     </div>
   )
 }
--- mens-ecommerce/src/Components/Navbar/Navbar.jsx
+++ mens-ecommerce/src/Components/Navbar/Navbar.jsx
@@ -1,22 +1,65 @@
-import React from 'react'
-import {navbarData} from './NavbarData';
+import React, { useState } from "react";
+import { navbarData } from "./NavbarData";
+import { ImCross } from "react-icons/im";
+import { FaBars } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { IoMdSearch } from "react-icons/io";
+import { FaShoppingCart } from "react-icons/fa";
+import Avatarmenu from "../LoginAvatar/AvatarMenu";
 //onClick={window.location.pathname = val.link}
-const Navbar = () => {
-  return (
-    <div className='sidebar'>
-        <ul className='sidebarList'>
-        {navbarData.map((val,key) => {
-            return(
-                <li key={key} className='item' >
-                    {" "} 
-                    <div style={{paddingRight:'6px'}}>{val.icon}</div>{" "}
-                    <div>{val.title}</div>
-                </li>
-            )
-        })}
-        </ul>
-    </div>
-  )
-}
 
-export default Navbar
+const Navbar = () => {
+  const [sidebar, setSidebar] = useState(false);
+  const showSidebar = () => setSidebar(!sidebar);
+
+  return (
+    <>
+      <div className="navbar">
+        <Link to="#" className="menu-bars">
+          <FaBars onClick={showSidebar} />
+        </Link>
+        <div className="searchInputs">
+          <div className="searchIcon">
+            <IoMdSearch />
+          </div>
+          <input
+            type="text"
+            placeholder="Search"
+            size="32"
+            className="textbox"
+          />
+        </div>
+        <div className="cart-icon">
+          <FaShoppingCart />
+        </div>
+    <div>
+        
+          <Avatarmenu />
+    </div>
+      </div>
+
+
+      <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+        <ul className="nav-menu-items" onClick={showSidebar}>
+          <li className="navbar-toggle">
+            <Link to="#" className="menu-bars">
+              <ImCross style={{ color: "#e3e2df", fontSize: "18px" }} />
+            </Link>
+          </li>
+          {navbarData.map((item, index) => {
+            return (
+              <li key={index} className={item.cname}>
+                <Link to={item.path}>
+                  {item.icon}
+                  <span>{item.title}</span>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;

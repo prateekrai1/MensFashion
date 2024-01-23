2f9cb0a3ef9159ce0cf46f97149fb7d3a042fd31
prateekrai1 <prateek@gmail.com>
2024-01-22 20:49:06 -0500

    Featured products done

---
+++ mens-ecommerce/src/Components/HomeBanner/HomeBanner.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HomeBanner = () => {
+  return (
+    <div className='banner-container'>
+        <div className='banner'>
+            <h1 className='banner-heading'>Be a MensFashion Member and earn Points and Rewards</h1>
+            <p className='banner-paragraph'>Not a member? Join now & get 10% off on every purchase.</p>
+        </div>
+    </div>
+  )
+}
+
+export default HomeBanner
